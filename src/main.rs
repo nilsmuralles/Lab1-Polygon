c56f9b9eec9f1201fa98daed64919b7746df1dd0
nilsmuralles <murallesnils11@gmail.com>
2025-07-05 15:37:09 -0600

    feat: testing functions

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,8 @@ fn main() {
     let mut fb = FrameBuffer::new(500, 500, Color::WHITE);
     fb.set_background_color(Color::BLUE);
     fb.clear();
+    fb.set_current_color(Color::RED);
+    fb.set_pixel(100, 100);
+    fb.set_pixel(200, 200);
     fb.render_to_file("result.png");
 }
