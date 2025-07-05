290d4ca1a9c2ef0b85104484688a9c46e5458de4
nilsmuralles <murallesnils11@gmail.com>
2025-07-04 19:56:25 -0600

    feat: Background color mutability

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -1,15 +1,15 @@
 use raylib::prelude::*;
 
 pub struct FrameBuffer {
-    pub width: u32,
-    pub height: u32,
+    pub width: i32,
+    pub height: i32,
     pub color_buffer: Image,
     background_color: Color,
     current_color: Color
 }
 
 impl FrameBuffer {
-    pub fn new(width: u32, height: u32, background_color: Color) -> Self {
+    pub fn new(width: i32, height: i32, background_color: Color) -> Self {
         let color_buffer = Image::gen_image_color(width, height, background_color);
         return FrameBuffer {
             width,
@@ -18,5 +18,32 @@ impl FrameBuffer {
             background_color,
             current_color: Color::WHITE
         }
+    }
+
+    pub fn clear(&mut self) {
+        self.color_buffer = Image::gen_image_color(self.width, self.height, self.background_color);
+    }
+
+    pub fn set_pixel(&mut self, x: i32, y: i32) {
+        if x > self.width || x < 0 {
+           println!("Out of bounds on x"); 
+        }
+        if y > self.height || y < 0 {
+           println!("Out of bounds on y"); 
+        }
+        self.color_buffer.draw_pixel(x, y, self.current_color);
+    }
+
+    pub fn set_background_color(&mut self, color: Color) {
+        self.background_color = color;
+    }
+
+    pub fn set_current_color(&mut self, color: Color) {
+        self.current_color = color;
+    }
+
+    pub fn render_to_file(&self, file_path: &str) {
+        self.color_buffer.export_image(file_path);
+        println!("Image saved successfully as '{}'!", file_path);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use framebuffer::FrameBuffer;
+use raylib::prelude::*;
+
 mod framebuffer;
 
 fn main() {
-    println!("Hello, world!");
+    let mut fb = FrameBuffer::new(500, 500, Color::WHITE);
+    fb.set_background_color(Color::BLUE);
+    fb.clear();
+    fb.render_to_file("result.png");
 }
